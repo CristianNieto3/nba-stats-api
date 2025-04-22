d8751dc4be89b0e980a4816b83f80721b185bbc9
CristianNieto3 <Cristian.nieto20@icloud.com>
2025-04-22 10:26:07 -0500

    Added new filtering methods and mock player data

--- nbastats/src/main/java/com/cristian/nbastats/player/PlayerController.java
+++ nbastats/src/main/java/com/cristian/nbastats/player/PlayerController.java
@@ -1,8 +1,6 @@
 package com.cristian.nbastats.player;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -10,16 +8,41 @@ import java.util.List;
 @RequestMapping(path = "/api/v1/players")
 public class PlayerController {
 
-    private final PlayerService playerService;
+    private final PlayerService playerService; //creates an instance of PlayerService
 
     public PlayerController(PlayerService playerService) {
         this.playerService = playerService;
     }
 
+    //Gets all players
     @GetMapping
     public List<Player> getAllPlayers() {
         return playerService.getAllPlayers();
+   }
 
-    }
+   @GetMapping ("/name/{name}")
+   public List<Player> getPlayerByName(@PathVariable String name) {
+        return playerService.getPlayerByName(name);
+   }
+
+    //How the HTTP on top will look - how you must use it
+   @GetMapping ("/team/{team}")
+    public List<Player> getPlayersByTeam(@PathVariable String team) {
+        return playerService.getPlayersByTeam(team);
+   }
+
+   //Gets each player by the position
+   @GetMapping ("/position/{position}")
+    public List<Player> getPlayersPosition(@PathVariable String position) {
+        return playerService.getPlayersPosition(position);
+   }
+
+ // @GetMapping ("/minppg")
+ // public List<Player> getMinPPG(@RequestParam(required = false, defaultValue = "0") double ppg ) {
+ //     return playerService.getMinPPG(ppg);
+ // }
+
+
+
 
 }
--- nbastats/src/main/java/com/cristian/nbastats/player/PlayerService.java
+++ nbastats/src/main/java/com/cristian/nbastats/player/PlayerService.java
@@ -1,14 +1,83 @@
 package com.cristian.nbastats.player;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PlayerService {
 
-    private final PlayerRepository playerRepository;
+    private List<Player> mockPlayers = new ArrayList<>();
 
-    @Autowired
+    public PlayerService() {
+        loadPlayersFromCsv();
+    }
+
+    //Returns ALL players
+    public List<Player> getAllPlayers() {
+        return mockPlayers;
+    }
+    //Search players by their name
+    public List<Player> getPlayerByName(String name) {
+        return mockPlayers.stream()
+                .filter(player -> player.getName().toLowerCase().contains(name.toLowerCase())).toList();
+    }
+   //Returns players with PPG >= users entered number
+   //public List<Player> getMinPpg(double ppg) {
+   //    return mockPlayers.stream()
+   //            .filter(player -> player.)
+   //}
+    //Returns all the players that play for a specific team.
+    public List<Player> getPlayersByTeam(String team) {
+        return mockPlayers.stream()
+                .filter(player -> player.getTeam().equalsIgnoreCase(team)).toList();
+    }
+    //Returns all the players that ara a certain position
+    public List<Player> getPlayersPosition(String position) {
+        return mockPlayers.stream()
+                .filter(player -> player.getPosition().equalsIgnoreCase(position)).toList();
+    }
+
+
+
+
+
+
+    private void loadPlayersFromCsv() {
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/nba_players.csv")))) {
+
+            String line;
+            boolean skipHeader = true;
+
+            while ((line = reader.readLine()) != null) {
+                if (skipHeader) {
+                    skipHeader = false;
+                    continue;
+                }
+                String[] fields = line.split(",");
+                Player player = new Player(Long.parseLong(fields[0]),
+                        fields[1],
+                        fields[2],
+                        fields[3],
+                        Double.parseDouble(fields[4]),
+                        Double.parseDouble(fields[5]),
+                        Double.parseDouble(fields[6]),
+                        Double.parseDouble(fields[7]),
+                        Double.parseDouble(fields[8]),
+                        Integer.parseInt(fields[9]));
+
+                mockPlayers.add(player);
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 }
