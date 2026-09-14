fd3ed491118df8e9602368b28060a94d027d0b8d
CristianNieto3 <cristian.nieto20@icloud.com>
2026-09-13 21:14:13 -0500

    Migrate backend to Spring Boot 4.1.1

--- nbastats/src/test/java/com/cristian/nbastats/player/PlayerServiceTest.java
+++ nbastats/src/test/java/com/cristian/nbastats/player/PlayerServiceTest.java
@@ -5,7 +5,7 @@ import com.cristian.nbastats.player.dto.PlayerResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 
